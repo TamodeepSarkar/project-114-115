8c7a1beaf3f848f1ebd7b88aa9198cb54902e0ed
TamodeepSarkar <86249468+TamodeepSarkar@users.noreply.github.com>
2022-05-11 14:32:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,11 +7,31 @@ function setup()
 {
     canvas=createCanvas(300,300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300.300);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function gotPoses(results)
+{
+    if (results.length>0)
+    {
+       console.log(results);
+       console.log("noseX = " + results[0].pose.nose.x)
+       console.log("noseY = " + results[0].pose.nose.y)
+    }
+}
+
+function modelLoaded()
+{
+    console.log('modelLoaded');
 }
 
 function draw()
 {
-
+  image(video,0,0,300,300);
 }
 
 function take_snapshot()
